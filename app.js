72d3636593dea63ff841afdeb6d8798fbfae90c5
olliegwb <184382533+olliegwb@users.noreply.github.com>
2024-12-17 15:55:11 +0000

    compelted

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ app.get("/quotes", async (req, res) => { //create get request. /quotes url.
 //needed to be an async function with await, as getQuotes is an async function, so this function was fulfulling before the getQuotes function, so returning an empty object.
 
 app.get("/quotes/:id", async (req, res) =>{ 
-const { id } = req.params; //take the ID from the URL
+  const { id } = req.params; //take the ID from the URL
   const quoteId = await getQuoteByID(id);
-res.status(200).send(quoteId);
+  res.status(200).send(quoteId);
 })
 
 
-/* This is what is given in the body.
+/* This is what is I type in the body in postman.
 {
   "quoteText": "This is a great quote",
   "author": "Ollie"
@@ -42,6 +42,20 @@ app.post("/quotes", async (req, res) => {
   const newQuote = await addQuote(newQuoteText, newAuthor); //passing the new text and new author into the function.
   res.status(201).send(newQuote);
 });
+
+app.patch("/quotes/:id", async (req, res) => {
+  const { id } = req.params; //needs to be in the curly brackets for some reason.
+  const newQuoteText = req.body.quoteText;
+  const newAuthor = req.body.author;
+  const editedQuote = await editQuote(id, newQuoteText, newAuthor);
+  res.status(200).send(editedQuote);
+})
+
+app.delete("/quotes/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedQuote = await deleteQuote(id);
+  res.status(200).send(deletedQuote);
+})
 
 app.listen(PORT, function () {
   console.log(`Server is now listening on http://localhost:${PORT}`);
